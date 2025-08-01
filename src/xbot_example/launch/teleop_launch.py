261c4cd930bf1f1fdb6d129440867144daff64bf
Clemens Elflein <clemens1@familie-elflein.de>
2025-08-01 10:06:10 +0200

    Added teleop_launch.py for xbot_example to configure and launch teleop-related nodes

---
+++ src/xbot_example/launch/teleop_launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'bind_ip_arg',
+            default_value=EnvironmentVariable('BIND_IP', default_value='0.0.0.0'),
+            description='Bind IP for the comms node. Set via BIND_IP environment variable.'
+        ),
+        Node(
+            package='xbot_comms',
+            executable='xbot_comms',
+            name='xbot_comms',
+            parameters=[{'bind_ip': LaunchConfiguration('bind_ip_arg')}]
+        ),
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node'
+        ),
+        Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_node',
+            parameters=[
+                {
+                    'enable_button': 0,
+                    'axis_linear.x': 1,
+                    'axis_linear.y': -1,
+                    'axis_linear.z': -1,
+                    'axis_angular.yaw': 0,
+                    'axis_angular.pitch': -1,
+                    'axis_angular.roll': -1
+                }
+            ]
+        )
+    ])
